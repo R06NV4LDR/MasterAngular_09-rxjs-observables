760eedb3851b344bef176b8a06a816fa070da45f
Ronny B Good <ronny.bruhin+bbc@hotmail.com>
2025-09-03 14:02:33 +0200

    1 second interval added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from "@angular/core";
+
+import { interval } from "rxjs";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  templateUrl: './app.component.html'
+  templateUrl: "./app.component.html",
 })
-export class AppComponent {
-
+export class AppComponent implements OnInit {
+  private destroyRef = inject(DestroyRef)
+  
+  ngOnInit(): void {
+    const subscription = interval(1000).subscribe({
+      next: (val) => console.log(val),
+    });
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+    // This is the full ngOnInit interval object which could take `next`, `complete` and `error` as parameters
+    // interval(1000).subscribe({
+    //   next: (val) => console.log(val),
+    //   complete: () => {},
+    //   error: () => console.log()
+  }
 }
